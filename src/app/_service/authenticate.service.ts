8c8dbc9872f39c099e9eac98b18b6d878bee72c4
Rowell David <yahikodavid27@gmail.com>
2021-03-05 14:55:44 +0800

    Added model and worked on service

--- src/app/_service/authenticate.service.ts
+++ src/app/_service/authenticate.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { User } from '../_model/user';
 
 @Injectable({
   providedIn: 'root'
@@ -7,9 +8,8 @@ export class AuthenticateService {
 
   constructor() { }
 
-  login(email: string, password: string) {
-    if (email && password) {
-      return alert("Login Success! Welcome "+email);
-    }
+  login(authData: User, success: any, error: any) {
+    if (authData) success(authData);
+    else error();
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
+import { invalid } from '@angular/compiler/src/render3/view/util';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { User } from '../_model/user';
 import { AuthenticateService } from '../_service/authenticate.service';
 
 @Component({
@@ -10,6 +12,8 @@ import { AuthenticateService } from '../_service/authenticate.service';
 export class LoginComponent implements OnInit {
   loginFormGrp: FormGroup;
   loader: boolean = false;
+  message: string = "";
+  messageType: string;
   
   constructor( private formBuilder: FormBuilder, private authenticateService: AuthenticateService) { }
 
@@ -29,9 +33,22 @@ export class LoginComponent implements OnInit {
   }
   
   onSubmitForm() {
-    // this.authenticateService.login(this.form.email.value, this.form.password.value)
-    //   .subscribe( response => {
-        
-    //   })
+    this.message = "";
+    this.loginFormGrp.markAllAsTouched();
+    if (this.loginFormGrp.invalid) return;
+    
+    let userData: User = {
+      "email" : this.form.email.value,
+      "password" : this.form.password.value
+    }
+
+    this.authenticateService.login(userData, 
+      success => {
+        this.message = "Account "+ success['email'] + " logged in successfully.";
+        this.messageType = "success";
+      }, error => {
+        this.message = "Invalid email or password."
+        this.messageType = "error";
+    })
   }
 }
